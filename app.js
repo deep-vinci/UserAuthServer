db781a8421ad02be035d4dbbd65ffb8b56f067e7
Deepak <deepakworkpc@gmail.com>
2025-01-11 22:41:42 +0530

    add the router

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cookieParser = require('cookie-parser')
 const crypto = require('crypto');
+const demo = require('./routes/demo')
 
-const db = require("./json-adapter");
-
-const app = express();
-app.use(cookieParser())
 
 const port = 49152;
 
+const app = express();
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true })); // For URL-encoded form data
+app.use(express.json()); // For JSON data (if applicable)
+
+
+app.use('/demo', demo);
 
 app.get("/", (req, res) => {
     // check for cookies if verified that cookie is valid than allow login
@@ -19,18 +23,22 @@ app.get("/", (req, res) => {
     res.send("");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signin", (req, res) => {
     // check for if email exists
     // if email exist then, send email already exist message
     // send cookie too with a validity of some time period
 })
 
-app.get("/signin", async (req, res) => {
-    // console.log(db.read());
-    res.send(await db.read());
+app.post("/signup", async (req, res) => {
+    console.log(req.body)
+    // console.log("db.read()");
+    // res.send(await db.read());
     // check if cookie is not there
     // check for emails password, if the user password matches email pass than allow login
-
+    // await db.read()
+    res.status(201).json({
+        message: 'User created successfully!',
+    });
 })
 
 
