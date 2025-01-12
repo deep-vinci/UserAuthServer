4532b8e7127b47a04af8998f7bb8cb2a95b6bbde
Deepak <deepakworkpc@gmail.com>
2025-01-12 10:06:01 +0530

    chore: add the routes under config

--- routes/signup.js
+++ routes/signup.js
@@ -1,29 +1,36 @@
-require('dotenv').config();
-
-
 const path = require("path");
-
 const express = require("express");
-const router = express.Router();
+const supabase = require("../config/dbClient");
 
+const router = express.Router();
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "signup.html"));
 });
 
 router.post("/", async (req, res) => {
+    
+    const { email } = req.body;
+
     try {
         const { data, error } = await supabase
-            .from('users') // Replace with your table name
-            .select('*');
+            .from('users')
+            .select('*')
+            .eq('email', email); // Filter by email
 
         if (error) throw error;
-        console.log(data);
+        
+        if (data[0].email == email) {
+            res.redirect("/signin");
+        } else {
+            res.redirect("/signup")
+        }
+        
     } catch (err) {
         console.error({ error: err.message });
     }
 
-    res.redirect("/signin");
+    
 });
 
 module.exports = router;
