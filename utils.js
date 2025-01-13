0c8dee2edc7427edb48fbca05196cb1b22a10b11
Deepak <deepakworkpc@gmail.com>
2025-01-13 22:34:16 +0530

    feat: add calculatable maxAge for cookie

--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
 const crypto = require('crypto');
 
+const calculateMaxAge = 1 * 60 * 60 * 1000; // 1 hour = 60 minutes * 60 seconds * 1000 milliseconds
+
 function generateToken() {
     return crypto.randomBytes(16).toString('hex');
 }
 
-module.exports = { generateToken };
+module.exports = { calculateMaxAge, generateToken };
