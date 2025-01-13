8725740f1b4872e91515dfd0090d5adceb58d9ff
Deepak <deepakworkpc@gmail.com>
2025-01-14 00:33:19 +0530

    fix: remove calculateMaxAge variable export

--- utils.js
+++ utils.js
@@ -1,9 +1,7 @@
 const crypto = require('crypto');
 
-const calculateMaxAge = 1 * 60 * 60 * 1000; // 1 hour = 60 minutes * 60 seconds * 1000 milliseconds
-
 function generateToken() {
     return crypto.randomBytes(16).toString('hex');
 }
 
-module.exports = { calculateMaxAge, generateToken };
+module.exports = { generateToken };
