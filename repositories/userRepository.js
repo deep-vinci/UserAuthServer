29b2ec9b8d52e94282b81d13ee37b3d287a7797a
Deepak <deepakworkpc@gmail.com>
2025-01-13 22:00:21 +0530

    feat: add createUser method under UserRepository class

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -3,16 +3,33 @@ const db = require('../config/dbClient');
 
 class UserRepository {
     
-    async doesEmailExistInDb(email) {
+    async findUserByEmail(email) {
         const { data, error } = await db
             .from('users')
             .select('*')
             .eq('email', email);
 
         if (error) throw error;
-
         return data.length > 0 ? data[0] : null;
+    }
+
+    async createUser(email, securePassword) {
+        const { data, error } = await db
+            .from('users')
+            .insert([{ email: email, password: securePassword }])
+            .select();
+
+        if (error) throw error;
+        return data[0];
     }
 }
 
 module.exports = new UserRepository();
+// new UserRepository().doesEmailExistInDb("b@example.com").then(data => {
+//     console.log(data);
+    // if(data) {
+    //     console.log(11)
+    // } else {
+    //     console.log(22)
+    // }
+// })
