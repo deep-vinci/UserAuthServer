dfd15939637e3b4e4c08fb411d2446b1d7b91311
Deepak <deepakworkpc@gmail.com>
2025-01-14 00:31:23 +0530

    chore: remove commented out code

--- app.js
+++ app.js
@@ -14,26 +14,10 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // For URL-encoded form data
 app.use(express.json()); // For JSON data (if applicable)
 
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use("/", require("./routes/app"));
 app.use('/demo', require('./routes/demo'));
 app.use('/signup', require('./routes/signup'));
 app.use('/signin', require('./routes/signin'));
-
-app.get("/", (req, res) => {
-    // check for cookies if verified that cookie is valid than allow login
-    console.log(req.cookies.sessionId)
-    res.cookie("sessionId", crypto.randomUUID(), {
-        maxAge: 1000 * 60 * 15
-    });
-    res.send("");
-});
-
-app.post("/signin", (req, res) => {
-    // check for if email exists
-    // if email exist then, send email already exist message
-    // send cookie too with a validity of some time period
-})
 
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
